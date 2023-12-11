84c4fb614791edd827731edf982a78613970e86f
Harshpoonia <97618527+Harshpoonia@users.noreply.github.com>
2023-12-11 20:44:13 +0530

    youtube_api.py

--- youtube_api.py
+++ youtube_api.py
@@ -1,12 +1,36 @@
-from googleapiclient.discovery import build
+from youtube_transcript_api import YouTubeTranscriptApi
+import config
 
-API_KEY = 'AIzaSyBS0Xeh4p4J1mPCOFK_FsIIP-jfZu6teIU'
+def getTranscript(video_id):
+    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=config.getConfig("languages"))
+    return transcript
 
-def get_video_details(video_id):
-    youtube = build('youtube', 'v3', developerKey=API_KEY)
-    request = youtube.videos().list(
-        part='snippet',
-        id=video_id
-    )
-    response = request.execute()
-    return response['items'][0] if response['items'] else None
+def getTranscriptText(video_id):
+    transcript = getTranscript(video_id)
+    if transcript is None:
+        raise Exception("Unable to find transcript")
+    
+    transcript_list = [""]
+    nb_char = 0
+    request_num = 0
+    for line in transcript:
+        nb_char += len(line['text']) + 1
+        if nb_char > 1000:
+            nb_char = 0
+            request_num += 1
+            transcript_list.append("")
+        
+        transcript_list[request_num] += line['text'] + " "
+    if request_num > 10:
+        raise Exception("Transcript is too long with length over " + str(request_num*1000) + " characters.")
+    return transcript_list
+
+def getVideoId(youtube_link):
+    video_id = youtube_link.split("v=")
+    if len(video_id) > 1:
+        return video_id[1]
+    video_id = youtube_link.split("youtu.be/")
+    if len(video_id) > 1:
+        return video_id[1]
+    
+    raise Exception("Unable to find video id")
