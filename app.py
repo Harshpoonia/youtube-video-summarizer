026394e2b173b5a0d8801911205330fec861fcb6
Harshpoonia <97618527+Harshpoonia@users.noreply.github.com>
2023-12-11 21:02:24 +0530

    app.py

--- app.py
+++ app.py
@@ -1,54 +1,34 @@
 import streamlit as st
-from youtube_api import get_video_details
+from youtube_util import get_video_id
 from text_summarization import summarize_text
-from text_to_speech import text_to_speech, translate_text
+from text_to_speech import text_to_speech
 
-def extract_video_id(link):
-    # Extract video ID from the YouTube link
-    # Example: https://www.youtube.com/watch?v=VIDEO_ID
-    parts = link.split("?v=")
-    if len(parts) > 1:
-        video_id = parts[1].split("&")[0]
-        return video_id
-    return None
+# Your YouTube API Key and other functions...
 
 def main():
     st.title("YouTube Video Summarizer")
 
-    # Link external CSS file
-    st.markdown(
-        """
-        <link href="styles.css" rel="stylesheet">
-        """,
-        unsafe_allow_html=True
-    )
-
     st.markdown("<p class='title'>YouTube Video Summarizer</p>", unsafe_allow_html=True)
 
     youtube_link = st.text_input("Enter YouTube video link:", key='youtube_link')
-    video_id = extract_video_id(youtube_link)
-
-    target_language = st.selectbox("Select Target Language", ["English", "Spanish", "French"], key='target_language')
+    video_id = get_video_id(youtube_link) if youtube_link else None
 
     if st.button("Summarize and Convert to Speech"):
-        if youtube_link:
-            # Fetch video transcript or content from YouTube API
-            video_content = get_video_details(video_id)  # Replace with actual fetched data from YouTube API
+        if video_id:
+            transcript = get_transcript(video_id)
+            if transcript:
+                # Summarize the fetched transcript
+                summary = summarize_text(transcript)
 
-            # Summarize the video content
-            summary = summarize_text(video_content)
+                # Convert summarized text to speech in English
+                text_to_speech(summary)
 
-            # Translate summarized text to the selected language
-            translated_summary = translate_text(summary, target_language.lower())
-
-            # Convert translated text to speech in English
-            text_to_speech(translated_summary)
-
-            st.success("Summarization and Conversion completed. Click below to listen.")
-            st.audio("output.mp3", format='audio/mp3')
-
+                st.success("Summarization and Conversion completed. Click below to listen.")
+                st.audio("output.mp3", format='audio/mp3')
+            else:
+                st.warning("Transcript not found for this video.")
         else:
-            st.warning("Please enter a YouTube video link.")
+            st.warning("Please enter a valid YouTube video link.")
 
 if __name__ == "__main__":
     main()
